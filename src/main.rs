0a4c37cf89fd4788069140d1665929f1b1d1d22c
TimRouze <timothe.rouze1@gmail.com>
2024-03-21 18:25:30 +0100

    - Improved Kmer handling - Changed data processing (should be better)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
 
+mod kmer;
 use clap::Parser;
 use flate2::Compression;
 use seq_io::fasta::{Reader, Record};
-use std::collections::{HashMap, BinaryHeap, BTreeSet};
+use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet};
 use std::sync::{Arc, Mutex, RwLock};
 use std::fs::File;
 use std::path::Path;
@@ -13,7 +14,11 @@ use std::env;
 use std::time::Instant;
 use::rayon::prelude::*;
 use flate2::write::GzEncoder;
+use bit_vec::BitVec;
 
+use kmer::{Kmer, RawKmer, Base};
+
+use crate::kmer::RevComp;
 
 const K: usize = 31;
 pub type KT = u64;
@@ -32,18 +37,12 @@ struct Args {
     ///Output directory
     #[arg(short, long, default_value_t = String::from(""))]
     out_dir: String,
-    /// Memory (in GB) allocated to Bloom filters (defaults to 4GB)
-    #[arg(short, long, default_value_t = 4)]
-    memory: usize,
     /// Number of hashes used in Bloom filters
     #[arg(short = 'H', long, default_value_t = 1)]
     hashes: usize,
     /// Seed used for hash functions
     #[arg(short, long, default_value_t = 101010)]
     seed: u64,
-    /// Modimizer ?
-    #[arg(short = 'M', long)]
-    modimizer: bool,
 }
 // TODO:
     // CREATE FOF INPUT FILE WITH LINK TO COMPRESSED FILES CONTAINING KMERS
@@ -53,7 +52,6 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let input_fof = args.input.as_str();
-    let size =  args.memory * 1_000_000_000;
     println!("FILENAME: {}", input_fof);
     let output_dir = args.out_dir;
     env::set_var("RAYON_NUM_THREADS", args.threads.to_string());
@@ -61,23 +59,30 @@ fn main() {
         let nb_files = lines_temp.count();
         let file = File::open(input_fof).unwrap();
         let reader = io::BufReader::new(file);
-        let vec_of_kmer_vec_mutex: Arc<Mutex<Vec<Vec<u64>>>> = Arc::new(Mutex::new(Vec::new()));
-        let color_names_mutex = Arc::new(Mutex::new(Vec::new()));
+        let kmer_map_mutex: Arc<Mutex<HashMap<u64, BitVec>>> = Arc::new(Mutex::new(HashMap::new()));
+        let file_number_mutex: RwLock<usize> = RwLock::new(0 as usize);
         reader.lines()
               .par_bridge()
               .for_each(|line| {
                 let filename = line.unwrap().clone();
                 println!("{}", filename);
-                let mut color_names = color_names_mutex.lock().unwrap();
-                color_names.push(extract_filename(&filename).unwrap().to_string());
-                let curr_vec = read_fasta(&filename);
-                let mut vec_of_kmer_vec = vec_of_kmer_vec_mutex.lock().unwrap();
-                vec_of_kmer_vec.push(curr_vec);
+                let curr_vec = read_fasta(&filename, &kmer_map_mutex, nb_files, &file_number_mutex);
+                let mut file_number = file_number_mutex.write().unwrap();
+                *file_number += 1;
+                drop(file_number);
               });
-        let vec_of_kmer_vecs = Arc::try_unwrap(vec_of_kmer_vec_mutex).expect("Failed to unwrap Arc").into_inner().expect("Failed to get Mutex");
-        let color_names = Arc::try_unwrap(color_names_mutex).expect("Failed to unwrap Arc").into_inner().expect("Failed to get Mutex");
-        let mut kmer_colors = process_vectors(&vec_of_kmer_vecs, &color_names);
-        for (color, kmer_map) in kmer_colors.iter_mut(){
+        let mut kmer_map = Arc::try_unwrap(kmer_map_mutex).expect("Failed to unwrap Arc").into_inner().expect("Failed to get Mutex");
+        let omnicolored_kmers = get_omnicolor(&mut kmer_map, nb_files);
+        let to_write_omnicolor = kmers_assemble(&RwLock::new(omnicolored_kmers.clone()));
+        let mut f = GzEncoder::new(File::create(output_dir.clone()+"omnicolor.fa.gz").expect("Unable to create file"), Compression::default());
+        println!("I write {} simplitigs from {} kmers when starting.", to_write_omnicolor.len(), omnicolored_kmers.len());                                                                                               
+        for i in &to_write_omnicolor{           
+            f.write(b">\n").unwrap();                                                                                                                                                       
+            f.write_all((*i).as_bytes()).expect("Unable to write data");
+            f.write(b"\n").unwrap();                                                                                                                         
+        } 
+        handle_other_colors(&mut kmer_map);
+        /*for (color, kmer_map) in kmer_colors.iter_mut(){
             println!("Reconstructing simplitigs for {} color", color);
             let before_kmers_assemble = Instant::now();
             let to_write = kmers_assemble(&Arc::new(RwLock::new(kmer_map.clone())));
@@ -91,120 +96,155 @@ fn main() {
                 f.write_all((*i).as_bytes()).expect("Unable to write data");
                 f.write(b"\n").unwrap();                                                                                                                         
             }     
-        }
+        }*/
     }
 }
 
-fn read_fasta(filename: &str) -> Vec<u64>{
+fn read_fasta(filename: &str, kmer_map_mutex: &Arc<Mutex<HashMap<u64, BitVec>>>, nb_files: usize, file_number: &RwLock<usize>){
     let ( reader, _compression) = niffler::get_reader(Box::new(File::open(filename).unwrap())).unwrap();
     let mut fa_reader = Reader::new(reader);
-    let mut kmer_vec = Vec::new();
+    let mut kmer = RawKmer::<K, KT>::new();
     while let Some(record) = fa_reader.next(){
         let record = record.expect("Error reading record");
-        for s in record.seq_lines(){
-            let seq = String::from_utf8_lossy(s);
-            if seq.len() >= 31{
-                for _i in 0..(seq.len()-K){
-                    let k_mer = str2num(&seq[_i.._i+K]);
-                    let canon = canon(k_mer, rev_comp(k_mer));
-                    kmer_vec.push(canon);
+        for (i, nuc) in record.seq().iter().filter_map(KT::from_nuc).enumerate(){
+            if i < K - 1{
+                kmer = kmer.extend(nuc);
+            }else{
+                kmer = kmer.append(nuc);
+                let canon = kmer.canonical().to_int();
+                let mut kmer_map = kmer_map_mutex.lock().unwrap();
+                let mut modified = false;
+                kmer_map.entry(canon).and_modify(|bit_vec|{
+                    bit_vec.set(*file_number.read().unwrap(), true);
+                    modified = true;
+                }).or_insert(BitVec::from_elem(nb_files, false));
+                if !modified{
+                    kmer_map.entry(canon).and_modify(|bit_vec| bit_vec.set(*file_number.read().unwrap(), true));
                 }
+                drop(kmer_map);
             }
         }
     }
-    kmer_vec.par_sort();
-    kmer_vec.dedup();
-    kmer_vec
 }
-
-fn process_vectors(vec_of_kmer_vecs:  &[Vec<u64>], color_names: &Vec<String>) -> HashMap<String, BTreeSet<u64>>{
-    println!("NB Files = {}", vec_of_kmer_vecs.len());
-    let mut indices = vec![0; vec_of_kmer_vecs.len()];
-    let mut kmer_heap = BinaryHeap::new();
-    let mut kmer_color: HashMap<String, BTreeSet<u64>> = HashMap::new();
-    let mut run = true;
-    let mut counter = 0;
-    vec_of_kmer_vecs.iter().for_each(|kmer_vec|{
-        kmer_heap.push(Reverse(kmer_vec[0]));
-    });
-    while run{
-        if let Some(min_kmer) = kmer_heap.pop(){
-            let mut new_color: String = String::from("");
-            for i in 0..vec_of_kmer_vecs.len(){
-                if indices[i] < vec_of_kmer_vecs[i].len() && min_kmer.0 == vec_of_kmer_vecs[i][indices[i]]{
-                    new_color += &(color_names[i].to_string()+"_");
-                    indices[i] += 1;
-                    counter += 1;
-                    if indices[i] < vec_of_kmer_vecs[i].len(){
-                        kmer_heap.push(Reverse(vec_of_kmer_vecs[i][indices[i]]));
+fn handle_other_colors(kmer_map:  &mut HashMap<u64, BitVec>){
+    println!("I will reconstruct simplitigs from {} kmers from different colors", kmer_map.len());
+    let nb_kmers = kmer_map.len();
+    let mut simplitigs = Vec::new();
+    let mut to_remove = Vec::new();
+    while let Some(pair) = get_first_key_value(kmer_map){
+        let key = pair.0;
+        let curr_color = pair.1;
+        to_remove.push(*key);
+        let mut forward = true;
+        let mut backward = true;
+        let mut simplitig = num2str(*key);
+        while forward{
+            let mut curr_kmer = RawKmer::<K, KT>::from_nucs(&simplitig[(simplitig.len()-K)..].as_bytes());
+            for succs in curr_kmer.successors(){
+                forward = false;
+                if kmer_map.contains_key(&succs.canonical().to_int()){
+                    let bit_vector = kmer_map.get(&succs.canonical().to_int()).unwrap();
+                    if bit_vector.eq(&curr_color) {
+                        simplitig.push(*succs.to_nucs().last().unwrap() as char);
+                        to_remove.push(succs.canonical().to_int());
+                        forward = true;
+                        break;
                     }
                 }
             }
-            if !new_color.is_empty(){
-                kmer_color
-                .entry(new_color)
-                .and_modify(|kmer_set|{ kmer_set.insert(min_kmer.0);})
-                .or_insert(BTreeSet::from([min_kmer.0]));
-            }
-        }else{
-            run = false;
         }
+        while backward{
+            let mut curr_kmer = RawKmer::<K, KT>::from_nucs(&simplitig[0..K].as_bytes());
+            for preds in curr_kmer.predecessors(){
+                backward = false;
+                if kmer_map.contains_key(&preds.canonical().to_int()){
+                    let bit_vector = kmer_map.get(&preds.canonical().to_int()).unwrap();
+                    if bit_vector.eq(&curr_color) {
+                        simplitig.insert(0,*preds.to_nucs().last().unwrap() as char);
+                        to_remove.push(preds.canonical().to_int());
+                        backward = true;
+                        break;
+                    }
+                }
+            }
+        }
+        for e in to_remove.iter(){
+            kmer_map.remove(e);
+        }
+        simplitigs.push(simplitig);
+        to_remove.clear();
     }
-    println!("NB kmer in 1 = {}", vec_of_kmer_vecs[0].len());
-    println!("NB kmer in 2 = {}", vec_of_kmer_vecs[1].len());
-    println!("I have seen {} matches!", counter);
-    kmer_color
+    println!("I have {} simplitigs from {} kmers when starting.", simplitigs.len(), nb_kmers);   
+}
+fn get_first_key_value(kmer_map:  &HashMap<u64, BitVec>) -> Option<(&u64, &BitVec)>{
+    kmer_map.iter().next()
 }
 
-fn kmers_assemble(kmers_mutex: &Arc<RwLock<BTreeSet<u64>>>) -> Vec<String>{
-    println!("I will reconstruct simplitigs from {} kmers", kmers_mutex.read().unwrap().len());
+fn get_omnicolor(kmer_map:  &mut HashMap<u64, BitVec>, nb_files: usize) -> BTreeSet<u64>{
+    let mut omnicolored_kmer: BTreeSet<u64> = BTreeSet::new();
+    let omni_vec = vec![true; nb_files];
+    for(key, val) in kmer_map.clone().iter(){
+        if val.eq_vec(&omni_vec){
+            omnicolored_kmer.insert(*key);
+            kmer_map.remove(key);
+        }
+    }
+    omnicolored_kmer
+}
+
+fn kmers_assemble(kmer_set_mutex: &RwLock<BTreeSet<u64>>) -> Vec<String>{
+    println!("I will reconstruct simplitigs from {} kmers", kmer_set_mutex.read().unwrap().len());
     let mut simplitigs = Vec::new();
-    while let Some(seed) = {
-        let mut kmers = kmers_mutex.write().unwrap();
-        kmers.pop_last()
+    while let Some(seed) =  {
+        let mut kmer_set = kmer_set_mutex.write().unwrap();
+        kmer_set.pop_last()
     }
     {
+        // println!("kmer = {}", num2str(seed));
         let mut simplitig = String::new();
-        //let before_max_simplitig = Instant::now();
-        let extended_simplitig = compute_max_from_kmer(kmers_mutex, &seed);
-        //println!("Assembly of 1 simplitig took: {}ms total.", before_max_simplitig.elapsed().as_micros());
+        let before_max_simplitig = Instant::now();
+        let extended_simplitig = compute_max_from_kmer(&kmer_set_mutex, &seed);
+        // println!("Assembly of 1 simplitig took: {}ms total.", before_max_simplitig.elapsed().as_micros());
         simplitig.push_str(&extended_simplitig);
         simplitigs.push(simplitig);
     }
     simplitigs
 }
 
-fn compute_max_from_kmer(available_kmers_mutex: &Arc<RwLock<BTreeSet<u64>>>, seed: &u64) -> String {
+fn compute_max_from_kmer(available_kmers: &RwLock<BTreeSet<u64>>, seed: &u64) -> String {
     let mut simplitig = num2str(*seed);
-    //let before_forward_canon = Instant::now();
-    let extended_simplitig = extend_forward(available_kmers_mutex, &mut simplitig);
-    //println!("Forward extension canonical left took: {}ms total.", before_forward_canon.elapsed().as_micros());
-    let mut reversed_simplitig = rev_comp_str(&extended_simplitig);
-    //let before_forward_revcomp = Instant::now();
-    simplitig = extend_forward(available_kmers_mutex, &mut reversed_simplitig);
-    //println!("Forward extension reverse way took: {}ms total.", before_forward_revcomp.elapsed().as_micros());
+    // let before_forward_canon = Instant::now();
+    // println!("Simplitig = {}", simplitig);
+    let extended_simplitig = extend_forward(available_kmers, &mut simplitig);
+    // println!("Extended simplitig = {}", extended_simplitig);
+    // println!("Forward extension canonical left took: {}ms total.", before_forward_canon.elapsed().as_micros());
+    let mut reversed_simplitig = rev_comp_str(&simplitig);
+    // let before_forward_revcomp = Instant::now();
+    simplitig = extend_forward(available_kmers, &mut reversed_simplitig);
+    // println!("Final simplitig = {}", simplitig);
+    // println!("Forward extension reverse way took: {}ms total.", before_forward_revcomp.elapsed().as_micros());
     simplitig
 }
 
-fn extend_forward(available_kmers_mutex: &Arc<RwLock<BTreeSet<u64>>>, simplitig: &mut String) -> String {
+fn extend_forward(available_kmers_mutex: &RwLock<BTreeSet<u64>>, simplitig: &mut String) -> String {
     let mut extend = true;
     let mask : u64 = ((1_u64) << (2*K))-1;
     while extend {
-        let mut query = str2num(&simplitig[(simplitig.len()-K)..]);
-        extend = false; // Set extend to false by default
-        for x in [b'A', b'C', b'G', b'T'].iter() {
-            query <<= 2;
-            query += nuc2int(x).unwrap();
-            query &= mask;
-            query = canon(query, rev_comp(query));
+        let mut query = RawKmer::<K, KT>::from_nucs(&simplitig[(simplitig.len()-K)..].as_bytes());
+        // println!("Query kmer = {}", String::from_utf8(query.to_nucs().to_ascii_uppercase()).unwrap());
+        // let mut query= RawKmer::<K, KT>::from_nucs(&simplitig[(simplitig.len()-K)..].as_bytes()).canonical();
+        extend = false;
+        for succs in query.successors(){
             let mut available_kmers = available_kmers_mutex.write().unwrap();
-            if available_kmers.contains(&query){
-                simplitig.push(*x as char);
-                available_kmers.remove(&query);
-                extend = true; // Set extend to true if a match is found
+            // println!("Candidate successor = {}", String::from_utf8(succs.to_nucs().to_ascii_uppercase()).unwrap());
+            if available_kmers.contains(&succs.canonical().to_int()){
+                // println!("Successor = {}", String::from_utf8(succs.to_nucs().to_ascii_uppercase()).unwrap());
+                simplitig.push(*succs.to_nucs().last().unwrap() as char);
+                available_kmers.remove(&succs.canonical().to_int());
+                extend = true;
                 break;
             }
-            query = str2num(&simplitig[(simplitig.len()-K)..]);
+            query = RawKmer::<K, KT>::from_nucs(&simplitig[(simplitig.len()-K)..].as_bytes()).canonical();
         }
     }
     simplitig.to_string()
@@ -344,9 +384,9 @@ fn num2str(mut k_mer: u64) -> String{
         }else if nuc == 1{
             res.push('C');
         }else if nuc == 2{//bebou
-            res.push('T');
-        }else if nuc == 3{
             res.push('G');
+        }else if nuc == 3{
+            res.push('T');
         }
         k_mer >>= 2;
     }
